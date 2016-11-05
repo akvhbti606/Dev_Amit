f478ae045ffc1c8dae51fd7b318e416461e4c919
akvhbti606 <akvhbti606@gmail.com>
2016-11-05 20:53:47 +0530

    new sop added

--- myfirstgitproject/src/com/java/cap/Demo.java
+++ myfirstgitproject/src/com/java/cap/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, This is demo for git project");
-		
+		System.out.println("Hi,this is second");
 	}
 
 }
